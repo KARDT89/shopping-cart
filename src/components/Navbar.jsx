f48bb59d624c40e57f3a6082962ce91ebdda45a4
DT89-lappy <dt89@rockzz.com>
2025-07-24 01:58:58 +0530

    add: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import ModeToggle from "./ModeToggle";
+import { ShoppingCart } from "lucide-react";
 
 export const Navbar = () => {
     return (
         <div>
-            <nav className="flex justify-between p-4 text-md bg-background text-primary font-mono ">
+            <nav className="flex justify-between p-4 text-md bg-background text-foreground font-mono ">
                 <h1 className="text-xl">My Store</h1>
-                <div className="flex gap-4 items-center justify-center text-primary/50">
+                <div className="flex gap-4 items-center justify-center text-foreground/50">
                     <NavLink
                         to="/"
                         className={({ isActive }) =>
-                            isActive ? "text-primary" : ""
+                            isActive ? "text-primary" : "hover:text-primary"
                         }
                     >
                         Home
@@ -19,11 +20,14 @@ export const Navbar = () => {
                     <NavLink
                         to="/products"
                         className={({ isActive }) =>
-                            isActive ? "text-primary" : ""
+                            isActive ? "text-primary" : "hover:text-primary"
                         }
                     >
                         Products
                     </NavLink>
+                </div>
+                <div className="flex items-center gap-4">
+                    <ShoppingCart className="hover:text-primary cursor-pointer"/>
                     <ModeToggle />
                 </div>
             </nav>
