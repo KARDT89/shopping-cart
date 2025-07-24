38f44ab48dc60ae8c964b9dec3c1104aaa89fc52
dt89mint <dt89@mint.com>
2025-07-24 13:36:30 +0530

    add: navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShoppingCart } from "lucide-react";
 export const Navbar = () => {
     return (
         <div>
-            <nav className="flex justify-between p-4 text-md bg-background text-foreground font-mono ">
+            <nav className="mx-auto lg:my-2 flex justify-between p-2.5 lg:px-8 w-full max-w-[100rem] text-md text-foreground bg-background font-mono border rounded-xl fixed top-0 left-0 right-0 dark:bg-transparent/80">
                 <h1 className="text-xl">My Store</h1>
                 <div className="flex gap-4 items-center justify-center text-foreground/50">
                     <NavLink
@@ -27,11 +27,14 @@ export const Navbar = () => {
                     </NavLink>
                 </div>
                 <div className="flex items-center gap-4">
-                    <ShoppingCart className="hover:text-primary cursor-pointer"/>
+                    <ShoppingCart className="hover:text-primary cursor-pointer" />
                     <ModeToggle />
                 </div>
             </nav>
+            <div className="py-16 px-4 lg:py-20 lg:px-10 bg-background h-screen  text-foreground">
+
             <Outlet />
+            </div>
         </div>
     );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from 'react'
+import { LoaderCircle } from 'lucide-react'
 import Card from './Card'
 
 export const Products = () => {
 
   const[products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(()=>{
     async function fetchProducts (){
+      setIsLoading(true)
       const response = await fetch('https://fakestoreapi.com/products')
       const data = await response.json()
       setProducts(data)
+      setIsLoading(false)
     }
     fetchProducts()
   },[])
+
+  console.log(products);
+  
+  if(isLoading) return (
+  <div className='flex h-full w-full items-center justify-center bg-background text-foreground text-2xl gap-2'>
+    <LoaderCircle className="animate-spin"/> <p>Loading</p>
+  </div>
+  )
   
   return (
     <div>
+      {/* left */}
+
+      {/* right */}
         {products.map((product)=>(
           <Card title={product.title}/>
         ))}
