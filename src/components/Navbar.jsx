adb59b046ded657d08e27ea4385ea8f1f34ea605
dt89 <tamalsarkartwoooo@gmail.com>
2025-07-25 21:14:21 +0000

    fix: navbar colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,30 +8,24 @@ export const Navbar = () => {
     <div>
       <nav className="mx-auto my-2 lg:my-4 block md:grid md:grid-cols-3 justify-start backdrop-blur-sm p-2.5 lg:px-8 w-[95%] max-w-[200rem] text-md text-white bg-black/80 font-mono border rounded-md fixed bottom-0 md:top-0 md:bottom-auto left-0 right-0 z-50">
         <h1 className="hidden md:block text-xl">Store89</h1>
-        <div className="hidden md:flex gap-4 items-center justify-center text-foreground/50 ">
+        <div className="hidden md:flex gap-4 items-center justify-center text-white/90 ">
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? 'text-primary' : 'hover:text-primary')}
+            className={({ isActive }) => (isActive ? 'text-white' : 'hover:text-primary')}
           >
             Home
           </NavLink>
           <NavLink
             to="/products"
-            className={({ isActive }) => (isActive ? 'text-primary' : 'hover:text-primary')}
+            className={({ isActive }) => (isActive ? 'text-white' : 'hover:text-primary')}
           >
             Shop
           </NavLink>
-          {/* <NavLink
-            to="/admin"
-            className={({ isActive }) => (isActive ? 'text-primary' : 'hover:text-primary')}
-          >
-            Admin
-          </NavLink> */}
         </div>
         <div className="grid grid-cols-5 justify-items-center md:flex md:items-center md:justify-end gap-4">
           <input
             type="text"
-            className="md:block bg-background border w-[180px] lg:w-full max-w-[250px] text-sm px-4 py-1 hidden"
+            className="md:block bg-black border w-[180px] lg:w-full max-w-[250px] text-sm px-4 py-1 hidden"
             placeholder="search..."
           />
 
@@ -58,3 +52,10 @@ export const Navbar = () => {
     </div>
   );
 };
+
+/* <NavLink
+to="/admin" 
+className={({ isActive }) => (isActive ? 'text-primary' : 'hover:text-primary')}
+>
+Admin
+</NavLink> */
